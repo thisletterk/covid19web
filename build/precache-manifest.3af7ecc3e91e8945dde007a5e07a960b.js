e6172698d4476f8f8a833f1756cb0a286e391ffe
rhmedias <rhmedias@users.noreply.github.com>
2021-03-22 14:03:30 +0100

    covid19 for web

---
+++ build/precache-manifest.3af7ecc3e91e8945dde007a5e07a960b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4fad85319f7b63b87a84bf3de2cc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41f3d4a1533a85626ad",
+    "url": "/static/css/main.5a0b5b58.chunk.css"
+  },
+  {
+    "revision": "6980a84d6f843fd8c946",
+    "url": "/static/js/2.38d3f1fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38d3f1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41f3d4a1533a85626ad",
+    "url": "/static/js/main.1b4b0881.chunk.js"
+  },
+  {
+    "revision": "4128aab56c1267f4e961",
+    "url": "/static/js/runtime-main.5d44d82b.js"
+  }
+]);
